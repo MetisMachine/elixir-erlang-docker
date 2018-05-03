6005ba03dbc9478325439874818ed13f1a8d312e
tsturzl <travissturzl@gmail.com>
2018-05-03 10:30:04 -0600

    fix entrypoint script

--- release/entrypoint.sh
+++ release/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if test "$(ls -A /etc/envconsul)"; then
-  echo "envconsul not configured, running without vault credentials"
-  exec "$@"
-else
   echo "envconsul configured, using configured credentials"
   exec /usr/local/bin/envconsul -config=/etc/vault-env "$@"
+else
+  echo "envconsul not configured, running without vault credentials"
+  exec "$@"
 fi
 
