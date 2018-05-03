9652fee8151b76983f60eae4e01e06ca1803c809
tsturzl <travissturzl@gmail.com>
2018-05-03 12:52:12 -0600

    fix exec statement

--- release/entrypoint.sh
+++ release/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 if [ "$(ls -A /etc/envconsul)" ]; then
-  CMD="/usr/local/bin/envconsul"
+  ARGS=""
   for i in `find /etc/envconsul -name *.hcl -type f`; do
     echo "Loading config: $i"
     DEREF=$(readlink -f $i)
-    CMD="$CMD -config=$DEREF"
+    ARGS="$ARGS -config=$DEREF"
   done
   echo "envconsul configured, using configured credentials"
-  exec "$CMD $@"
+  exec /usr/local/bin/envconsul "$ARGS $@"
 else
   echo "envconsul not configured, running without vault credentials"
   exec "$@"
