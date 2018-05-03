0bff213d6ddb4d66243d404b8b04e3d6cd5ee85a
tsturzl <travissturzl@gmail.com>
2018-05-03 10:33:47 -0600

    go away typo

--- release/entrypoint.sh
+++ release/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$(ls -A /etc/envconsul)" ]; then
   echo "envconsul configured, using configured credentials"
-  exec /usr/local/bin/envconsul -config=/etc/vault-env "$@"
+  exec /usr/local/bin/envconsul -config=/etc/envconsul "$@"
 else
   echo "envconsul not configured, running without vault credentials"
   exec "$@"
