d346263deeebb7cdfe757250026223b69e8c4e39
tsturzl <travissturzl@gmail.com>
2018-05-04 09:30:40 -0600

    use an array for args

--- release/entrypoint.sh
+++ release/entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 if [ "$(ls -A /etc/envconsul)" ]; then
-  ARGS=""
+  ARGS=()
   for i in `find /etc/envconsul -name *.hcl -type f`; do
     echo "Loading config: $i"
     DEREF=$(readlink -f $i)
-    ARGS="$ARGS -config=$DEREF"
+    ARGS+=("-config=$DEREF")
   done
+  ARGS+=("$@")
   echo "envconsul configured, using configured credentials"
-  exec /usr/local/bin/envconsul "$ARGS $@"
+  exec /usr/local/bin/envconsul "${ARGS[@]}"
 else
   echo "envconsul not configured, running without vault credentials"
   exec "$@"
